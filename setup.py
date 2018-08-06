ba936a75c41eab4bc5677c0587e2d4286ebbb438
Evgeny Taranov <taranov@jet.msk.su>
2018-08-06 13:30:56 +0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+        name='descision_trees',
+        version='0.1',
+        packages=['clickstream',
+                  'charrec'],
+        url='',
+        license='MIT',
+        author='Evgeny Taranov',
+        author_email='allista@gmail.com',
+        description=''
+)
