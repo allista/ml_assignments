a3dd89a0d4a0b6aa2831385b5343e7d9f0ac2da2
Evgeny Taranov <taranov@jet.msk.su>
2018-08-27 18:13:21 +0300

    Tests and optimizations of ID3

--- clickstream/main.py
+++ clickstream/main.py
@@ -13,67 +13,76 @@ class Tree:
         self.label = label
 
     @staticmethod
-    def _split(features, labels, attribute):
+    def _split(features, label, attribute):
         subs = {}
         attr = features[attribute]
         for val in attr.unique():
-            sub_features = features[attr == val].drop(columns=[attribute])
-            sub_labels = labels.loc[sub_features.index]
-            subs[val] = (sub_features, sub_labels, Counter(sub_labels))
+            sub_features = features[attr == val]
+            subs[val] = (sub_features, Counter(sub_features[label]))
         return subs
 
     @classmethod
-    def _best_attr(cls, features, labels, classes, alpha):
-        best_attr = [-1, None, None]
+    def _best_attr(cls, used_attrs, features, label, classes, alpha):
+        best_attr = (-1, None, None)
         S = entropy(list(classes.values()))
-        total = float(len(labels))
+        total = float(len(features.index))
         for attr in features:
+            if attr in used_attrs:
+                continue
             gain = S
-            subs = cls._split(features, labels, attr)
-            # if cls._should_stop(labels, classes, subs, alpha):
-            #     continue # 0.26584
-            for val, (sub_f, sub_l, sub_c) in subs.items():
-                gain -= len(sub_l) / total * entropy(list(sub_c.values()))
+            subs = cls._split(features, label, attr)
+            for val, (sub_f, sub_c) in subs.items():
+                gain -= len(sub_f.index) / total * entropy(list(sub_c.values()))
             if gain > best_attr[0]:
-                best_attr = [gain, attr, subs]
-        if cls._should_stop(labels, classes, best_attr[2], alpha):
-            return -1, None, None  # 0.25376
+                best_attr = (gain, attr, subs)
+        if cls._should_stop(total, classes, best_attr[2], alpha):
+            return -1, None, None
         return best_attr
 
     @staticmethod
-    def _should_stop(labels, classes, subs, alpha):
-        total = float(len(labels))
+    def _should_stop(total, classes, subs, alpha):
         cnames = sorted(classes)
         observed = []
         expected = []
-        for val, (sub_features, sub_labels, sub_count) in subs.items():
-            sub_len = len(sub_labels)
+        for val, (sub_features, sub_count) in subs.items():
+            sub_len = len(sub_features.index)
             observed.append([sub_count[k] for k in cnames])
             expected.append([classes[k] * sub_len / total for k in cnames])
         chi, pv = chisquare(observed, expected, axis=None)
-        return pv >= alpha
+        return pv > alpha
 
     def _subtree(self, val):
         sub_tree = Tree(self)
         self.children[val] = sub_tree
         return sub_tree
 
-    def induce(self, features, labels, classes=None, alpha=0.05):
+    def _induce(self, features, label, alpha, classes=None, used_attrs=None):
         if classes is None:
-            classes = Counter(labels)
+            classes = Counter(features[label])
+        if used_attrs is None:
+            used_attrs = {label}
         self.label = classes.most_common(1)[0][0]
         if len(classes) == 1 or features.shape[1] == 0:
             print('Stopped with the most common class: {}'
                   .format(classes.most_common()))
         else:
-            gain, self.attribute, subs = self._best_attr(features, labels, classes, alpha)
+            gain, self.attribute, subs = self._best_attr(used_attrs,
+                                                         features, label, classes, alpha)
             if self.attribute is None:
                 print('Stopped by Chi2 with the most common class: {}'
                       .format(classes.most_common()))
             else:
+                used_attrs = used_attrs | {self.attribute}
                 print('Splitting at "{}" with gain {}'.format(self.attribute, gain))
-                for val, (sub_features, sub_labels, sub_classes) in subs.items():
-                    self._subtree(val).induce(sub_features, sub_labels, sub_classes, alpha)
+                for val, (sub_features, sub_classes) in subs.items():
+                    self._subtree(val)._induce(sub_features, label,
+                                               alpha, sub_classes, used_attrs)
+
+    @classmethod
+    def induce(cls, features, label, alpha):
+        tree = cls()
+        tree._induce(features, label, alpha)
+        return tree
 
     def evaluate(self, row):
         if not self.children:
@@ -84,8 +93,9 @@ class Tree:
             return child.evaluate(row)
         return self.label
 
-    def error_rate(self, features, labels):
-        total = len(labels)
+    def error_rate(self, features, label):
+        labels = features[label]
+        total = len(features.index)
         return sum(self.evaluate(features.iloc[i]) != labels.iloc[i]
                    for i in range(total)) / float(total)
 
@@ -129,6 +139,9 @@ if __name__ == '__main__':
     import os
 
 
+    # import sys
+    # from cProfile import Profile
+
     def read_data(basename, names=None):
         return pd.read_csv('../data/clickstream/{}.csv'.format(basename),
                            sep=' ', names=names)
@@ -136,16 +149,41 @@ if __name__ == '__main__':
 
     names = read_data('featnames', ['name'])
     trainfeat = read_data('trainfeat', names.name)
-    trainlabs = read_data('trainlabs', ['result'])
+    trainfeat['label'] = read_data('trainlabs', ['result']).result
     testfeat = read_data('testfeat', names.name)
-    testlabs = read_data('testlabs', ['result'])
+    testfeat['label'] = read_data('testlabs', ['result']).result
 
-    savefile = 'clickstream-005.json'
+    savefile = 'clickstream-100.json'
     if os.path.isfile(savefile):
         t = Tree.load(savefile)
     else:
-        t = Tree()
-        t.induce(trainfeat, trainlabs.result, alpha=0.05)
+        # prof = Profile()
+        # prof.enable()
+        t = Tree.induce(trainfeat, 'label', alpha=1)
+        # prof.disable()
+        # prof.dump_stats('induce.prof')
         t.save(savefile)
-    print('Train error rate: {}'.format(t.error_rate(trainfeat, trainlabs.result)))
-    print('Test error rate: {}'.format(t.error_rate(testfeat, testlabs.result)))
+    print('Train error rate: {}'.format(t.error_rate(trainfeat, 'label')))
+    print('Test error rate: {}'.format(t.error_rate(testfeat, 'label')))
+
+# 1
+# Train error rate: 0.0512
+# Test error rate: 0.3366
+# 0.5
+# Train error rate: 0.180325
+# Test error rate: 0.27168
+# 0.2
+# Train error rate: 0.1862
+# Test error rate: 0.2664
+# 0.1
+# Train error rate: 0.1871
+# Test error rate: 0.26464
+# 0.05
+# Train error rate: 0.1877
+# Test error rate: 0.25376
+# 0.01
+# Train error rate: 0.188875
+# Test error rate: 0.24916
+# 0.001
+# Train error rate: 0.191525
+# Test error rate: 0.24956
